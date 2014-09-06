f267ea9d95c0dcc0cd75a5cfd6cf37e50a24ceb9
skarmani2002 <skamrani2002@gmail.com>
2014-09-06 17:11:47 +0500

    Create bundle for Shopify

---
+++ src/Shopify/ShopifyBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Shopify\ShopifyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return new response("test");
+    }
+}
---
+++ src/Shopify/ShopifyBundle/ShopifyShopifyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Shopify\ShopifyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShopifyShopifyBundle extends Bundle
+{
+}
